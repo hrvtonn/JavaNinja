14b3b764d2abd9dabeec20d32038e8b1e86fc239
hrvtonn <heryvelton20@gmail.com>
2024-12-21 10:29:49 -0300

    Primeiro commit exercicios

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,18 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        long SaldoBancario = 9999999L;
+
+        System.out.println(idade);
+        System.out.println(SaldoBancario);
+        System.out.println(altura);
+        System.out.println("Saldo Bancario é: " + SaldoBancario);
+        System.out.println("Minha idade é: " + idade);
+    }
+}
